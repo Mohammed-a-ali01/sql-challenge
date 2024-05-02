0dacf21b062591eacf2620b6823e41778175099b
Ali <Mohammed.a.ali01@outlook.com>
2024-05-02 18:16:56 -0400

    main

---
+++ EmploymentSQL/TableSchema.sql
@@ -0,0 +1,67 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/hiiDbB
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+-- Modify this code to update the DB schema diagram.
+-- To reset the sample schema, replace everything with
+-- two dots ('..' - without quotes).
+
+CREATE TABLE "employee" (
+    "emp_no" int   NOT NULL,
+    "emp_title_id" varchar(20)   NOT NULL,
+    "birth_date" date   NOT NULL,
+    "first_name" varchar(30)   NOT NULL,
+    "last_name" varchar(30)   NOT NULL,
+    "sex" varchar(2)   NOT NULL,
+    "hire_date" date   NOT NULL,
+    CONSTRAINT "pk_employee" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "departments" (
+    "dept_no" varchar(10)   NOT NULL,
+    "deptname" varchar(30)   NOT NULL,
+    CONSTRAINT "pk_departments" PRIMARY KEY (
+        "dept_no"
+     )
+);
+
+CREATE TABLE "department_emp" (
+    "emp_no" int   NOT NULL,
+    "dept_no" varchar(20)   NOT NULL
+);
+
+CREATE TABLE "department_manager" (
+    "dept_no" varchar(30)   NOT NULL,
+    "emp_no" int   NOT NULL
+);
+
+CREATE TABLE "salaries" (
+    "emp_no" int   NOT NULL,
+    "salary" int   NOT NULL
+);
+
+CREATE TABLE "titles" (
+    "title_id" varchar(10)   NOT NULL,
+    "title" varchar(30)   NOT NULL
+);
+
+ALTER TABLE "employee" ADD CONSTRAINT "fk_employee_emp_title_id" FOREIGN KEY("emp_title_id")
+REFERENCES "titles" ("title_id");
+
+ALTER TABLE "department_emp" ADD CONSTRAINT "fk_department_emp_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employee" ("emp_no");
+
+ALTER TABLE "department_emp" ADD CONSTRAINT "fk_department_emp_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "department_manager" ADD CONSTRAINT "fk_department_manager_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "department_manager" ADD CONSTRAINT "fk_department_manager_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employee" ("emp_no");
+
+ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employee" ("emp_no");
+
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # sql-challenge
 Company employment during 1980's and 1990's 
+
+
+# Task 
+Task if to complete a research project about people who are employed during a certain time. We will import the CSV files, import them inot a SQL database and answer the questions.
+
+# Schema
+![alt text](ERD-schema.jpg)
+
+Our Schema shows the relationships between our entities. From this we can better understand the relationship between them.
+
+# Tables 
+[text](EmploymentSQL/TableSchema.sql)
+
+
+# Queries Ouput
+
+
